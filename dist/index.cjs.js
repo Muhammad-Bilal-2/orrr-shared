40aef7340cfe1a005cf1de2cb23011c87141d5d3
Muhammad Bilal Ashraf <bashraf@orrprotection.net>
2021-07-27 15:27:21 +0500

    Commited the package

---
+++ dist/index.cjs.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var MyComponent = function (_a) {
+    var text = _a.text, color = _a.color;
+    return (React__default['default'].createElement("div", null,
+        "Bilal ",
+        text,
+        " ",
+        color));
+};
+
+exports.MyComponent = MyComponent;
